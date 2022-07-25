a95cde8acd439489b6456e1f68069920aa6192b7
Andy Glover <andrew.l.glover@gmail.com>
2022-07-25 14:59:26 -0700

    Add stylesheet and gif link

---
+++ js/script.js
@@ -0,0 +1,4 @@
+let baseurl = "http://www.andyglover.dev/gifs/"
+let gifname;
+gifContainer = document.querySelector('#gifContainer');
+gifContainer.setAttribute('src', `${baseurl}/${gifname}.gif`);
