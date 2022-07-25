28e6d366c8e444de1547dbda7d2025e533734f3e
Andy Glover <andrew.l.glover@gmail.com>
2022-07-25 15:06:45 -0700

    Add functional gif link

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-let baseurl = "http://www.andyglover.dev/gifs/"
+let baseurl = "http://www.andyglover.dev/gifs"
 let gifname;
-gifContainer = document.querySelector('#gifContainer');
-gifContainer.setAttribute('src', `${baseurl}/${gifname}.gif`);
+
+const gifLink = document.querySelector('#gifLink');
+const gifContainer = document.querySelector('#gifContainer');
+
+function updateImgSrc(baseurl,gifname) {
+    gifContainer.setAttribute('src', `${baseurl}/${gifname}.gif`);
+}
+
+gifLink.addEventListener('click', updateImgSrc(baseurl,gifname));
